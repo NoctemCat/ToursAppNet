efb3355daf0df594839a65f617518e1c0766a5da
NoctemCat <ya7kolya@gmail.com>
2021-10-04 12:09:19 +0900

    Add project files.

---
+++ ToursAppNet/Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace ToursAppNet
+{
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
